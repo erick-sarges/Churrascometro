3e9f80c264106a26360c441d33665f980c9cc478
erick <erick.sarges0223@gmail.com>
2022-08-08 18:46:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Carne - 400 gr por pessoa + 6 horas - 650
+// Cerveja  - 1200 ml por pessoa  + 6 horas - 2000 ml
+// Refrigerante/Agua - 1000 ml por pessoa + 6 horas 1500ml
+
+// Criança valem por 0,5 
+
